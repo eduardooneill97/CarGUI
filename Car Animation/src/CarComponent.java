733808631e50445f2b06382ae596481b00cafd90
eduar <eduar@10.21.63.227>
2017-11-07 18:53:20 -0400

    Adding movement in line

--- Car Animation/src/CarComponent.java
+++ Car Animation/src/CarComponent.java
@@ -4,12 +4,13 @@ import java.awt.Graphics2D;
 import javax.swing.JComponent;
 
 public class CarComponent extends JComponent {
-	
+	Car car1 = new Car(0,0);
 	public void paintComponent(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
 		
-		Car car1 = new Car(getWidth()-60,getHeight()-30);
-		car1.draw(g2);	
+		
+		car1.draw(g2);
+		car1.setxPos(car1.getxPos()+2);
 		
 	}
 
--- Car Animation/src/CarViewer.java
+++ Car Animation/src/CarViewer.java
@@ -1,16 +1,17 @@
 import javax.swing.JFrame;
 
 public class CarViewer {
-	public static void main(String[]args) {
+	public static void main(String[]args) throws InterruptedException {
 		JFrame frame = new JFrame();
 		frame.setSize(600, 600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		//CarComponent component = new CarComponent();
-		
-		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+		CarComponent component = new CarComponent();		
 		frame.add(component);		
 		frame.setVisible(true);
+		while(true) {
+			frame.repaint();
+			Thread.sleep(20);
+		}
 		
 	}
 
